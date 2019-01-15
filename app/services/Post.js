96e67b055be9d07c3284d4870f1bacac44383c4f
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 11:24:21 -0300

    Added getOneById feature to Post service

--- app/services/Post.js
+++ app/services/Post.js
@@ -1,9 +1,14 @@
 import ApiConfig from '../config/Api'
 import ApiGetCollectionHelper from '../helpers/ApiGetCollection'
+import ApiGetOneByIdHelper from '../helpers/ApiGetOneById'
 
 const PostService = {
   getAll: function (successCallbackFunc, errorCallbackFunc) {
     return ApiGetCollectionHelper(ApiConfig.POSTS_PATH, successCallbackFunc, errorCallbackFunc)
+  },
+
+  getOneById: function (postId, successCallbackFunc, errorCallbackFunc) {
+    return ApiGetOneByIdHelper(ApiConfig.POSTS_PATH, postId, successCallbackFunc, errorCallbackFunc)
   }
 }
 
