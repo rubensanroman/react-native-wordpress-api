a83dc8b62525030d9dc638f04ec6ad1999fc617b
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 16:51:25 -0300

    Added ApiAuth service

---
+++ app/services/ApiAuth.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import WpCredentialsConfig from '../config/WpCredentials'
+import ApiGetAuthTokenHelper from '../helpers/ApiGetAuthToken'
+
+const ApiAuthService = {
+  handshake: function () {
+    ApiGetAuthTokenHelper(WpCredentialsConfig.admin, this.callbackSuccessFunc, this.callbackErrorFunc)
+  },
+
+  callbackSuccessFunc: function (response) {
+    if (response.token !== undefined) {
+      WpCredentialsConfig.authToken = response.token
+    }
+  },
+
+  callbackErrorFunc: function (error) {
+    console.error(error)
+  }
+}
+
+export default ApiAuthService
