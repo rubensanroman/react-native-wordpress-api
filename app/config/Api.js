f02980ce854bd00f39ebfaef575d59b9a1afe30f
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 05:13:31 -0300

    Now Navigator.js uses constants

---
+++ app/config/Api.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+module.exports = {
+  URL: 'http://localhost',
+  BASE_PATH: '/wp-json/wp/v2',
+  POSTS_PATH: '/posts',
+  PAGES_PATH: '/pages',
+  USERS_PATH: '/users',
+  MEDIA_PATH: '/media'
+}
--- app/config/Navigator.js
+++ app/config/Navigator.js
@@ -5,19 +5,19 @@ import AboutScreen from '../screens/About'
 import ContactScreen from '../screens/Contact'
 
 module.exports = {
-  Home: {
+  HOME: {
     screen: HomeScreen,
     navigationOptions: {
       tabBarIcon: ({ tintColor }) => <Ionicons name='md-home' size={32} color={tintColor} />
     }
   },
-  About: {
+  ABOUT: {
     screen: AboutScreen,
     navigationOptions: {
       tabBarIcon: ({ tintColor }) => <Ionicons name='md-contact' size={32} color={tintColor} />
     }
   },
-  Contact: {
+  CONTACT: {
     screen: ContactScreen,
     navigationOptions: {
       tabBarIcon: ({ tintColor }) => <Ionicons name='md-mail' size={32} color={tintColor} />
