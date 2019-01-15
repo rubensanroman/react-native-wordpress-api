ed0146539183a725827070212a7f514471314a08
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 10:59:51 -0300

    Added some stuff to About screen for testing purposes

--- app/screens/About/index.js
+++ app/screens/About/index.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import PostService from '../../services/Post'
+import ErrorScreen from '../Error'
 
 class AboutScreen extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      data: []
+      data: [],
+      isLoading: true
     }
   }
 
+  componentDidMount () {
+    PostService.getAll(this.dataDidLoad, this.dataDidLerror)
+  }
+
+  dataDidLoad = (data) => {
+    console.log(data);
+    this.setState({ data, isLoading: false })
+  }
+
+  dataDidError = () => {
+    this.setState({ data: <ErrorScreen />, isLoading: false })
+  }
+
   render () {
-    return (
+    return (this.state.isLoading) ? (
       <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center' }}>Welcome to the About Screen !</Text>
+        <Text style={{ textAlign: 'center' }}>Loadings ...</Text>
+      </View>
+    ) : (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text style={{ textAlign: 'center' }}>{ JSON.stringify(this.state.data) }</Text>
       </View>
     )
   }
