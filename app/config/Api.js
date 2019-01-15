ef35ba5bf05e27807d1c841653e34421a41c9bf6
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 16:53:20 -0300

    Added JWT_AUTH_PATH to Api config file

--- app/config/Api.js
+++ app/config/Api.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 module.exports = {
   URL: 'http://localhost',
+  JWT_AUTH_PATH: '/wp-json/jwt-auth/v1/token',
   BASE_PATH: '/wp-json/wp/v2',
   POSTS_PATH: '/posts',
   PAGES_PATH: '/pages',
