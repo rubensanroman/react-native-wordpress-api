8786009a630692aeea70e601c9a43dfb4ad714f9
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 16:51:55 -0300

    Initialize ApiAuth service on the very beginning of the app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
 import NavigatorConfig from './config/Navigator'
+import ApiAuthService from './services/ApiAuth'
+
+ApiAuthService.handshake()
 
 const Navigator = createBottomTabNavigator(NavigatorConfig)
 
