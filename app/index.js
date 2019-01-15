340f307954683f2279b304dfe379664889d507ec
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-14 21:33:47 -0300

    Import all the screens from app/index

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import HomeScreen from './screens/Home'
+import AboutScreen from './screens/About'
+import ContactScreen from './screens/Contact'
 
-export default () => <HomeScreen />
+export default () => <ContactScreen />
