d5782fac74022a6ad62778b4581e67b2471bf0a0
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-14 21:40:37 -0300

    The first navigation 'hello world' working like a charm :)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import HomeScreen from './screens/Home'
 import AboutScreen from './screens/About'
 import ContactScreen from './screens/Contact'
 
-export default () => <ContactScreen />
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  About: AboutScreen,
+  Contact: ContactScreen
+})
+
+export default createAppContainer(TabNavigator)
