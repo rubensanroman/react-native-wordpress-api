5c33daa81786b1a998777085d6b98d6fcfd69125
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 10:58:58 -0300

    Fixed Post service

--- app/services/Post.js
+++ app/services/Post.js
@@ -2,8 +2,8 @@ import ApiConfig from '../config/Api'
 import ApiGetCollectionHelper from '../helpers/ApiGetCollection'
 
 const PostService = {
-  getAll: function () {
-    return ApiGetCollectionHelper(ApiConfig.POSTS_PATH)
+  getAll: function (successCallbackFunc, errorCallbackFunc) {
+    return ApiGetCollectionHelper(ApiConfig.POSTS_PATH, successCallbackFunc, errorCallbackFunc)
   }
 }
 
