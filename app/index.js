95ab1202f9616cfc83a872e1cfd8aa1db8fa2364
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-14 22:15:14 -0300

    Added Expo's Ionicons to the navigation tabs

--- app/index.js
+++ app/index.js
@@ -1,12 +1,29 @@
+import React from 'react'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import { Ionicons } from '@expo/vector-icons'
 import HomeScreen from './screens/Home'
 import AboutScreen from './screens/About'
 import ContactScreen from './screens/Contact'
 
 const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  About: AboutScreen,
-  Contact: ContactScreen
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name='md-home' size={32} color='black' />
+    }
+  },
+  About: {
+    screen: AboutScreen,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name='md-contact' size={32} color='black' />
+    }
+  },
+  Contact: {
+    screen: ContactScreen,
+    navigationOptions: {
+      tabBarIcon: () => <Ionicons name='md-mail' size={32} color='black' />
+    }
+  }
 })
 
 export default createAppContainer(TabNavigator)
