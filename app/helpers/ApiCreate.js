779f3013718a9da586242c4bf4d18e1a7be3cebd
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-15 13:59:19 -0300

    Now ApiCreate helper can  create :)

--- app/helpers/ApiCreate.js
+++ app/helpers/ApiCreate.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ApiConfig from '../config/Api'
+import WpCredentialsConfig from '../config/WpCredentials'
 
 const ApiCreateHelper = (itemsPath, itemBody, successCallback, errorCallback) => {
   return fetch(ApiConfig.URL + ApiConfig.BASE_PATH + itemsPath, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + WpCredentialsConfig.authToken
     },
     body: JSON.stringify(itemBody)
   })
