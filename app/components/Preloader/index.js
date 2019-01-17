8c9bdcbc236fe76f6cee10fc99d933cc3e05fbfe
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-17 17:00:34 -0300

    Rename preloader image

--- app/components/Preloader/index.js
+++ app/components/Preloader/index.js
@@ -3,7 +3,7 @@ import { Image } from 'react-native'
 import Styles from './styles'
 
 const PreloaderComponent = () => (
-  <Image source={require('./image_preloader.gif')} style={Styles.preloader} />
+  <Image source={require('./image-preloader.gif')} style={Styles.preloader} />
 )
 
 export default PreloaderComponent
