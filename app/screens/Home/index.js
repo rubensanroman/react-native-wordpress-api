04231187b6ab94619439c3cdd589873c2469a8a6
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-17 09:07:54 -0300

    Home screen fetchs all posts by default

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, FlatList, Text, View } from 'react-native'
+import PostService from '../../services/Post'
+import ContainerComponent from '../../components/Container'
+import PreloaderComponent from '../../components/Preloader'
 
 class HomeScreen extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      data: []
+      data: [],
+      isLoading: false
     }
   }
 
+  componentDidMount () {
+    this.getAllPosts()
+  }
+
+  getAllPosts () {
+    PostService.getAll(this.dataDidLoad, this.dataDidError)
+
+    this.setState({ isLoading: true })
+  }
+
+  dataDidLoad = (response) => {
+    this.setState({ data: response.data, isLoading: false })
+  }
+
+  dataDidError = (error) => {
+    console.log('Ups! Hubo un fucking error!')
+
+    this.setState({ isLoading: false })
+  }
+
   render () {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text style={{ textAlign: 'center' }}>Welcome to the Home Screen !</Text>
-      </View>
+    return (this.state.isLoading) ? ( <ContainerComponent children={ <PreloaderComponent /> } /> ) : (
+      <ContainerComponent>
+        {
+          <FlatList
+            data={ this.state.data }
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) =>
+              <View>
+                <Text>{item.title.rendered}</Text>
+                <Text>{item.content.rendered}</Text>
+              </View>
+            }
+            />
+        }
+      </ContainerComponent>
     )
   }
 }
