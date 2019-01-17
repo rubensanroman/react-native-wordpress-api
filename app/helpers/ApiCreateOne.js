812eefce0ce0b516a9a0d0fd755d4e6970c43c54
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-17 06:56:53 -0300

    ApiCreateOne triggers the api request after the authToken is obtained

--- app/helpers/ApiCreateOne.js
+++ app/helpers/ApiCreateOne.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { AsyncStorage } from 'react-native'
 import ApiConfig from '../config/Api'
 import WpCredentialsConfig from '../config/WpCredentials'
 import ApiRequestHelper from './ApiRequest'
 
 const ApiCreateOneHelper = (itemsPath, itemData, successCallbackFunc, errorCallbackFunc) => {
-  return ApiRequestHelper(ApiConfig.URL + ApiConfig.BASE_PATH + itemsPath, 'post', itemData, WpCredentialsConfig.authToken, successCallbackFunc, errorCallbackFunc)
+  return AsyncStorage.getItem('authToken').then(authToken => {
+    ApiRequestHelper(ApiConfig.URL + ApiConfig.BASE_PATH + itemsPath, 'post', itemData, authToken, successCallbackFunc, errorCallbackFunc)
+  })
 }
 
 export default ApiCreateOneHelper
