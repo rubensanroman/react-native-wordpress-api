4dfac0494abaaded2b4365f835cb78933b900fb6
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-17 03:43:51 -0300

    Didn't remember that wpCredentials were passed as an argument

--- app/helpers/ApiGetAuthToken.js
+++ app/helpers/ApiGetAuthToken.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import axios from 'axios'
 import ApiConfig from '../config/Api'
-import WpCredentialsConfig from '../config/WpCredentials'
 
 const ApiGetAuthTokenHelper = (wpCredentials, successCallback, errorCallback) => {
   return axios.post(ApiConfig.URL + ApiConfig.JWT_AUTH_PATH,
-    JSON.stringify(WpCredentialsConfig.admin),
+    wpCredentials,
     {
       headers: {
         'Accept': 'application/json',
