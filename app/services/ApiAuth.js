a46302d486b827151076436a853bcb0b29397bc0
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-20 02:01:04 -0300

    Simplified ApiAuth service

--- app/services/ApiAuth.js
+++ app/services/ApiAuth.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import { AsyncStorage } from 'react-native'
-import WpCredentialsConfig from '../config/WpCredentials'
 import ApiGetAuthTokenHelper from '../helpers/ApiGetAuthToken'
 
 const ApiAuthService = {
-  getAuthToken: function () {
-    ApiGetAuthTokenHelper(WpCredentialsConfig.admin, this.callbackSuccessFunc, this.callbackErrorFunc)
-  },
-
-  callbackSuccessFunc: function (response) {
-    AsyncStorage.setItem('authToken', response.data.token)
-  },
-
-  callbackErrorFunc: function (error) {
-    console.error(error)
+  getAuthToken: function (wpCredentials, callbackSuccessFunc, callbackErrorFunc) {
+    return ApiGetAuthTokenHelper(wpCredentials, callbackSuccessFunc, callbackErrorFunc)
   }
 }
 
