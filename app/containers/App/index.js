4eee32d66e8b3aa135f1164f1bb776e9cf38aaca
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-22 09:28:28 -0300

    Moved Container component to /containers folder as App container

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { StatusBar, View } from 'react-native'
 import Styles from './styles'
 
-const ContainerComponent = ({ children }) => (
+const AppContainer = ({ children }) => (
   <View style={Styles.container}>
     <StatusBar hidden />
     {children}
   </View>
 )
 
-export default ContainerComponent
+export default AppContainer
