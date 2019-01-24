671063bbd217510b39d9894150ceab970aefa95c
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-24 02:32:44 -0300

    By ApiAuthService from main index

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
 import ScreensConfig from './config/Screens'
-import ApiAuthService from './services/ApiAuth'
 
 const Navigator = createBottomTabNavigator(ScreensConfig)
 
