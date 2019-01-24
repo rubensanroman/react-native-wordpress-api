12a45572b199d4a57f3c64b00390e5afb699ba9f
Ruben Sanromán <bitbucket@rubensanroman.com>
2019-01-24 01:15:21 -0300

    And more changes, every big change has some victims

--- app/helpers/ApiGetAuthToken.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ApiConfig from '../config/Api'
-import ApiRequestHelper from './ApiRequest'
-
-const ApiGetAuthTokenHelper = (wpCredentials, successCallbackFunc, errorCallbackFunc) => {
-  return ApiRequestHelper(ApiConfig.URL + ApiConfig.ENDPOINTS.JWT_AUTH, 'post', wpCredentials, '', successCallbackFunc, errorCallbackFunc)
-}
-
-export default ApiGetAuthTokenHelper
